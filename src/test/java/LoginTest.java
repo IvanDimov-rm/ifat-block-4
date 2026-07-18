7d9ef70ec1a28ba28d2f95c877d5c7cd8c7079ba
IvanDimov <Ivandimov981pushok@gmail.com>
2026-07-18 12:43:51 +0400

    First test is updated

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,17 +1,9 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertTrue;
-import static org.testng.AssertJUnit.assertEquals;
-
 public class LoginTest {
-    //1. открыть нужный браузер
-    //2. зайти на сайт saucedemo.com
-    //3. залогиниться
-    //4. проверить что залогинились
 
     @Test
     public void login() {
